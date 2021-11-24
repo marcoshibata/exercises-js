ce936da66aea3e26ef7117d89aa4a801609b6776
Marco Shibata <66227743+marcoshibata@users.noreply.github.com>
2021-11-24 23:24:17 +0900

    acrecentando imagens

--- Hora do dia/script.js
+++ Hora do dia/script.js
@@ -1,1 +1,21 @@
-window.alert("Ola")
+function carregar () {
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("image")
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora>= 0 && hora < 12){
+        //bom dia
+        img.src = `image/manha.png`
+        document.body.style.background=`#daded0`
+    } else if (hora>=12 && hora <18){
+        //boa tarde
+        img.src = `image/tarde.png`
+        document.body.style.background=`#ffcf5e`
+    }else{
+        //boa noite
+        img.src = `image/noite.png`
+        document.body.style.background=`#6e9fc7`
+    }
+}
+
