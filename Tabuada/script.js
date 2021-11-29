2a3861410be12bdb06c45cd45665790d746252d5
Marco Shibata <66227743+marcoshibata@users.noreply.github.com>
2021-11-29 10:44:44 +0900

    funcionando

--- Tabuada/script.js
+++ Tabuada/script.js
@@ -5,8 +5,27 @@ function contar(){
     var res = document.getElementById("res")
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert("[ERRO] Faltam dados!")
+        res.innerHTML = `Impossivel contar!`
+        //window.alert("[ERRO] Faltam dados!")
     }else{
-        res.innerHTML = "Contandto..."
+        res.innerHTML = "Contandto...<br>" 
+        var i = Number(ini.value)  // converte os valores em numero
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if(p<=0){ // se o passo for "0" , considera como passo 1
+            window.alert(`Passo inválido, considerando " passo 1"`)
+            p=1
+        }
+
+        if(i<f){ // contagem crescente
+        for(var c=i;c<=f;c+=p){  // o "for" irá fazer a contagem
+            res.innerHTML += `${c} \u{1F449}` // acrescentando emoji ( unicode emoji list)
+        }
+    }else{ // contagem regressiva
+        for(var c= i; c>= f;c-=p){
+            res.innerHTML += `${c} \u{1F449}` 
+        }
+    }
+    res.innerHTML += `\u{1F3C1}` // insere bandeira no final da contagem
     }
 }
