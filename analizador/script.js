0cccf728f95d93e6ee3f8f6637ebaf86e4c07835
Marco Shibata <66227743+marcoshibata@users.noreply.github.com>
2021-12-06 22:32:08 +0900

    finalizando

--- analizador/script.js
+++ analizador/script.js
@@ -22,8 +22,39 @@ function inLista(n,l){
 
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        window.alert(`Tudo Ok`)
+        valores.push(Number(num.value)) // adiciona ao vetor "valores"
+        let item = document.createElement(`option`) // cria option dentro do select
+        item.text = `Valor ${num.value} adicionado` // valor do item sera um texto
+        lista.appendChild(item) // adiciona a lista
+        res.innerHTML=`` // limpar
     }else{
         window.alert(`Valor invalido ou ja encontrado na lista`)
     }
+    num.value =``  //limpa o campo
+    num.focus()    // define o foco ao campo
 }
+function finalizar(){
+        if(valores.length == 0){
+            window.alert(`Adicione valores`)
+        }else{
+            let tot = valores.length
+            let maior = valores[0] // no inicio o primeiro valor eh o maior
+            let menor = valores[0] // no inicio o primeiro valor tambem eh o menor
+            let soma = 0
+            let media = 0
+            for(let pos in valores){
+                soma += valores[pos]
+                if(valores[pos]> maior)
+                maior = valores[pos]
+                if(valores[pos]<menor)
+                menor = valores[pos]
+            }
+            media = soma/tot
+            res.innerHTML= ``
+            res.innerHTML+= `<p>Ao todo temos ${tot} numeros cadastrados</p>`
+            res.innerHTML+= `<p>O maior valor adicionado foi ${maior}</p>`
+            res.innerHTML+= `<p>O menor valor adicionado foi ${menor}.</p>`
+            res.innerHTML+= `<p>Somando todos os valores , temos ${soma}</p>`
+            res.innerHTML+= `<p>A media doas valores digitados e ${media}</p>`
+        }
+    }
