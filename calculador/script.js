347eaaf44701f8230cd5065b92023019948d313d
Marco Shibata <66227743+marcoshibata@users.noreply.github.com>
2021-12-07 23:51:03 +0900

    consertando tudo

--- calculador/script.js
+++ calculador/script.js
@@ -1,27 +1,7 @@
-let result = document.getElementById(`resultado`)
-let element= document.getElementById(`elementos`)
-let observando = document.getElementById(`observando`)
-let transicao = ``
-let visor = ``
-let calculando =``
-function numero1(){
-    transicao +=`1`
-    visor +=`1`
-    result.innerHTML=transicao
-    element.innerHTML=visor
+function inserir(valor){
+    let val = document.getElementById(`resultado`).innerHTML
+    document.getElementById(`resultado`).innerHTML=val + valor
 }
-function numero2(){
-    transicao +=`2`
-    visor +=`2`
-    result.innerHTML=transicao
-    element.innerHTML=visor
-}
-function somar(){
-    if(transicao != ``){
-    visor+=`+`
-    calculando = Number(calculando)+Number(transicao)
-    transicao=``
-    element.innerHTML=visor
-    result.innerHTML=calculando
-    }
+function limpar(){
+    document.getElementById(`resultado`).innerHTML=``
 }
