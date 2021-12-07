1084d4ccd1a84c911cf99e5367ee6e88de9ba704
Marco Shibata <66227743+marcoshibata@users.noreply.github.com>
2021-12-08 00:10:31 +0900

    tudo funcionando

--- calculador/script.js
+++ calculador/script.js
@@ -1,7 +1,18 @@
+
 function inserir(valor){
     let val = document.getElementById(`resultado`).innerHTML
     document.getElementById(`resultado`).innerHTML=val + valor
 }
 function limpar(){
     document.getElementById(`resultado`).innerHTML=``
+}
+function voltar(){
+    let resul = document.getElementById(`resultado`).innerHTML
+    document.getElementById(`resultado`).innerHTML = resul.substring(0,resul.length -1)
+}
+function calcular(){
+    let result = document.getElementById(`resultado`).innerHTML
+    if(result){
+        document.getElementById(`resultado`).innerHTML = eval(result)
+    }
 }
