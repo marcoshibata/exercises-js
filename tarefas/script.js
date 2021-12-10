a822b7d83099018bbd67d5686cb903392abf341a
Marco Shibata <66227743+marcoshibata@users.noreply.github.com>
2021-12-10 17:06:51 +0900

    agora funciona

--- tarefas/script.js
+++ tarefas/script.js
@@ -1,8 +1,7 @@
 `use strict`;
 let banco = [  // simulnado banco de dados
     {'tarefa':'Estudar','status':''},
-    {'tarefa':'Netflix','status':'checked'},
-    {'tarefa':'Amazon1','status':'checked'}
+    {'tarefa':'Netflix','status':'checked'}
 ]
 
 const criarItem  = (tarefa, status) => {
@@ -13,28 +12,29 @@ const criarItem  = (tarefa, status) => {
     <div>${tarefa}</div>
     <input type="button" value="x">
     `
-    document.getElementById(`todoList`).appendChild(item)
+    document.getElementById('todoList').appendChild(item)
 }
-const limparTarefas = () =>{ // nao permite que duplique dados quando atulizar tela dfor chamado
+
+const limparTela = () => {
     const todoList = document.getElementById('todoList')
     while (todoList.firstChild){
         todoList.removeChild(todoList.lastChild)
     }
 }
-
 const atualizarTela = () => { // cada vez que banco for atualizado, atualiza a tela
-    limparTarefas()
+    limparTela()
     banco.forEach(item => criarItem(item.tarefa,item.status))
 }
 
-const inserirItem = (evento) =>{
+const inserirItem = (evento) => {
     const tecla = evento.key
-    if (tecla === 'Enter'){
-        banco.push({'tarefa':'Criar desenho','status':''})
+    const texto = evento.target.value
+    if ( tecla === 'Enter'){
+        banco.push({'tarefa':texto,'status':''})
         atualizarTela()
+        evento.target.value = ''
     }
 }
-
-document.getElementById('newItem').addEventListener('Keypress',nserirItem)
+document.getElementById('newItem').addEventListener('keypress',inserirItem)
 
 atualizarTela()
